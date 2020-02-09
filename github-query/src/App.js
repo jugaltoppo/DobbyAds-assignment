16d6fee36e60a577c4b20787728819c4affd25c2
jugaltoppo <jugalsekhartoppo@gmail.com>
2020-02-09 13:40:44 +0530

    initial commit

---
+++ github-query/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component, useState, useEffect } from 'react';
+
+const App = () => {
+  let [userData, setUserQuery] = useState([]);
+  let [searchQuery, setSearchQuery] = useState("");
+  
+  const fetchdata = () => { 
+    fetch(`https://api.github.com/search/users?q=${searchQuery}`)
+    .then((body) => {
+      return body.json();
+    })
+    .then((data) => {
+      console.log(data);
+      setUserQuery(data.items)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+  
+  useEffect(() => {
+    fetchdata()
+  }, [searchQuery]);
+  
+  const newQuery = (e) => {
+    setSearchQuery(e.target.value)
+  }
+  
+  return (
+    <div>
+    <h1>User search results</h1>
+    <input type="text" onChange={newQuery} value={searchQuery}></input>
+    {userData ? userData.map((n, i) => {
+      return (
+        <div key={i}>
+        <p>{n.login}</p>
+        <img src={n.avatar_url} style={{height: "100px"}}></img>
+        <p><a href={n.url}>{n.login}'s profile</a></p>
+        <p><a href={n.repos_url}>{n.login}'s repositories</a></p>
+        <hr></hr>
+        </div>);
+      }) : <p>type something</p>
+    }
+    </div>
+    );
+  }
+  
+  
+  
+  
+  // class App extends Component {
+  //   render() {
+  //     return 
+  //   }
+  // }
+  
+  export default App;
+  
